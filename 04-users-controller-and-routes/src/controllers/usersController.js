6bbe4426179b52b5b39e64d83d51aafceeabc57e
Alex Britto <alexsander01@hotmail.com.br>
2026-01-31 07:32:15 -0300

    feat: implement body parsing and user creation endpoint

--- 04-users-controller-and-routes/src/controllers/usersController.js
+++ 04-users-controller-and-routes/src/controllers/usersController.js
@@ -23,4 +23,22 @@ module.exports = {
     }
     res.send(200, user);
   },
+
+  createUser(req, res) {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("end", () => {
+      body = JSON.parse(body);
+      lastUserId = users[users.length - 1].id;
+      const newUser = {
+        id: lastUserId + 1,
+        name: body.name,
+      };
+      users.push(newUser);
+      res.send(201, newUser);
+    });
+  },
 };
