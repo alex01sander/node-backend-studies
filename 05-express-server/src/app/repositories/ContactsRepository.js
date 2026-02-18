ecbd62c5cd21aa27cd870b0d5e9946a279617b65
Alex Britto <alexsander01@hotmail.com.br>
2026-02-18 08:46:57 -0300

    refactor: migrate contact update to PostgreSQL

--- 05-express-server/src/app/repositories/ContactsRepository.js
+++ 05-express-server/src/app/repositories/ContactsRepository.js
@@ -43,13 +43,6 @@ class ContactsRepository {
         return row[0];
     }
 
-    delete(id) {
-        return new Promise((resolve) => {
-            contacts = contacts.filter((contact) => contact.id !== id);
-            resolve();
-        });
-    }
-
     async create({ name, email, phone, category_id }) {
         const row = await db.query(
             `INSERT INTO contacts (name, email, phone, category_id) VALUES ($1, $2, $3, $4) RETURNING *`,
@@ -58,19 +51,18 @@ class ContactsRepository {
         return row[0];
     }
 
-    update(id, { name, email, phone, category_id }) {
+    async update(id, { name, email, phone, category_id }) {
+        const row = await db.query(
+            `UPDATE contacts SET name = $1, email = $2, phone = $3, category_id = $4 WHERE id = $5 RETURNING *`,
+            [name, email, phone, category_id, id],
+        );
+        return row[0];
+    }
+
+    delete(id) {
         return new Promise((resolve) => {
-            const updateContact = {
-                id,
-                name,
-                email,
-                phone,
-                category_id,
-            };
-            contacts = contacts.map((contact) =>
-                contact.id === id ? updateContact : contact,
-            );
-            resolve(updateContact);
+            contacts = contacts.filter((contact) => contact.id !== id);
+            resolve();
         });
     }
 }
