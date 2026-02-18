3c8ab42a60db60e3f93b12f5885c9a2c56fd5a90
Alex Britto <alexsander01@hotmail.com.br>
2026-02-18 07:04:37 -0300

    refactor: migrate contact find methods to PostgreSQL

--- 05-express-server/src/app/repositories/ContactsRepository.js
+++ 05-express-server/src/app/repositories/ContactsRepository.js
@@ -20,20 +20,23 @@ let contacts = [
 ];
 
 class ContactsRepository {
-    findAll() {
-        return new Promise((resolve) => resolve(contacts));
+    async findAll() {
+        const rows = await db.query(`SELECT * FROM contacts`);
+        return rows;
     }
 
-    findById(id) {
-        return new Promise((resolve) =>
-            resolve(contacts.find((contact) => contact.id === id)),
-        );
+    async findById(id) {
+        const row = await db.query(`SELECT * FROM contacts WHERE id = $1`, [
+            id,
+        ]);
+        return row[0];
     }
 
-    findByEmail(email) {
-        return new Promise((resolve) =>
-            resolve(contacts.find((contact) => contact.email === email)),
-        );
+    async findByEmail(email) {
+        const row = await db.query(`SELECT * FROM contacts WHERE email = $1`, [
+            email,
+        ]);
+        return row[0];
     }
 
     delete(id) {
