063a919cf90a083aabc9a9f98ce1cab0c31fee29
Alex Britto <alexsander01@hotmail.com.br>
2026-02-19 07:42:21 -0300

    feat: implement categories controller and repository

---
+++ 05-express-server/src/app/controllers/CategoryController.js
@@ -0,0 +1,18 @@
+const CategoryRepository = require("../repositories/CategoryRepository");
+class CategoryController {
+  async index(req, res) {
+    const categories = await CategoryRepository.findAll();
+    res.json(categories);
+  }
+
+  async store(req, res) {
+    const { name } = req.body;
+    if (!name) {
+      return res.status(400).json({ error: "Name is required" });
+    }
+    const category = await CategoryRepository.create({name});
+    res.json(category);
+  }
+}
+
+module.exports = new CategoryController();
